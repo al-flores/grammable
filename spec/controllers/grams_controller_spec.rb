87a1ab060a9b301ca3e7a98bd6c2d20760cbafbb
Jose Flores <alfredo_flores123@yahoo.com>
2018-08-28 02:40:36 +0000

    error rsepc

---
+++ spec/controllers/grams_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe GramsController, type: :controller do
+    decsribe "grams#index action" do
+    it "should successfully show the page" do
+    end
+
+end
+
+end
