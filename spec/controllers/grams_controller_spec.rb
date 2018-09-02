4dfdb0e53c01e75326af7474cba482a611584813
Jose Flores <alfredo_flores123@yahoo.com>
2018-09-02 02:51:20 +0000

    the destory action was added

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,19 +2,23 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
-  describe "grams#edit action" do
-  it "should successfully show the edit form if the gram is found" do
-    gram = FactoryBot.create(:gram)
-    get :edit, params: { id: gram.id }
-    expect(response).to have_http_status(:success)
+  describe "grams#destroy" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
 
+    end
+
+    it "should return a 404 message if we cannot find a gram with the id that is specified" do 
+      delete :destroy, params: { id: 'SPACEDUCK' }
+      expect(response).to have_http_status(:not_found)
+      
+    end
   end
 
-  it "should return a 404 error message if the gram is not found" do
-    get :edit, params: { id: 'SWAG' }
-    expect(response).to have_http_status(:not_found)
-  end
-end
 
 
   describe "grams#update action" do
